49215cf1fa4c137eca2dcbd7cb645259e0b7d819
Amir Raofi <staramir@gmail.com>
2015-08-08 11:56:40 -0700

    Create public folder and move images there

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var app = express();
 // Set up the port to listen on
 app.set('port', 8080);
 
+// Configure the routes
+app.use(express.static(__dirname + '/public')); // Route for public resources: images/css/js
+
 // Start the listen server
 app.listen(app.get('port'), function () {
     console.log('Server has been started on port ' + app.get('port'));
